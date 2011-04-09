fd15da29403848041f05fbc4d982b9d24ca3903c
Dongling Ding <dling61@yahoo.com>
2011-04-08 17:18:57 -0700

    add contact about page

---
+++ forgotpw.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Forgot</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="login">
+  <div id="loginright">
+    <div id="logintitle"><img src="images/forgot.gif" width="550" height="65" /><br />
+    </div>
+    <div id="loginaera"><br />
+      <div id="loginline">
+        <div id="loginname">Your Email: </div>
+        <div id="loginput">
+          <input type="text" class="loginaera"/>
+        </div>
+      </div>
+    </div>
+    <div id="loginbuttom">
+      <div id="loginbuttomimg"><a href="#"><img src="images/Retrieve.gif" width="150" height="24" /></a></div>
+    </div>
+  </div>
+  <div id="signcpright">Questions? Contact Customer Service at: 1-800-555-1234(US) <br />
+    Copyright © 2011 E2Wstudy.com LLC All rights reserved </div>
+</div>
+</body>
+</html>
